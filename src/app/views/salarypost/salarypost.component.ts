6f70902876ef9309152954e1a33680d6d13fc7de
cyclone77 <fillchar@live.com>
2017-12-30 19:09:20 +0800

    选择表册弹框

--- src/app/views/salarypost/salarypost.component.ts
+++ src/app/views/salarypost/salarypost.component.ts
@@ -7,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SalarypostComponent implements OnInit {
 
-  private title = '镶边表';
+  moduleTitle = '镶边表';
+  tblDlg = {
+    title: '标准表选择',
+    toggle: false,
+    datalist: [{
+      name: '岗位薪点标准表(全民、大集体)'
+    }, {
+      name: '岗位薪点标准表(自聘)'
+    }],
+    selectedTblName: ''
+  };
   constructor() { }
 
   ngOnInit() {
+    this.tblDlg.selectedTblName = this.tblDlg.datalist[0].name;
   }
 
+  selectTable() {
+    this.tblDlg.toggle = true;
+  }
+
+  closeTblDlg() {
+    this.tblDlg.toggle = false;
+  }
+
+  useTable(tblname) {
+    this.tblDlg.selectedTblName = tblname;
+    this.tblDlg.toggle = false;
+  }
 }
